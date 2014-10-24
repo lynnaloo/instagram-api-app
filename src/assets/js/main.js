e9b1bc1dfaa27ac01de9e71b20b3af8826c11f79
Linda Nichols <lynnaloo@gmail.com>
2014-10-24 04:20:19 +0000

    fixed jquery depends

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,14 +1,21 @@
 require.config({
     paths: {
-      'jquery': 'libs/jquery/jquery.min.js'
+      'jquery': 'libs/jquery/jquery',
+      'jquery-instagram': 'libs/jquery-instagram/dist/instagram'
     },
     shim: {
-
+      'jquery-instagram' : {
+          deps: ['jquery']
+      },
     }
 });
 
-require(['jquery'], function() {
+require(['jquery', 'jquery-instagram'], function ($) {
 
-    console.log($('html'));
+  $(".instagram").instagram({
+    userId: 'lynnaloo',
+    clientId: '4c1191c3ee9040b9968f432f2c977964',
+    image_size:'standard_resolution'
+  });
 
 });
