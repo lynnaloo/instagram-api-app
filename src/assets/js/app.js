7d1ae3667515ee6536986683bfb691094c17a503
Linda Nichols <lynnaloo@gmail.com>
2014-10-27 04:55:40 +0000

    resolves #8

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,20 +1,15 @@
 var App = {};
 
-App.Config = {
-  client: '4c1191c3ee9040b9968f432f2c977964',
-  urlRoot: 'https://api.instagram.com/v1/'
-};
-
-
 (function () {
 
   function fetchPhotos(filter) {
-    var hash = 'norfolkva',
+    var hash = filter,
+      // TODO: move to config
       id = '23299063',
       client = '4c1191c3ee9040b9968f432f2c977964',
-      count = 50,
-      url = 'https://api.instagram.com/v1/tags/' + hash + '/media/recent?client_id=' +
-      client + '&count=' +   count;
+      count = 100,
+      url = 'https://api.instagram.com/v1/users/' + id + '/media/recent?client_id=' +
+        client + '&count=' + count;
 
     $.ajax({
       method: "GET",
@@ -22,20 +17,24 @@ App.Config = {
       dataType: "jsonp",
       cache: false,
       success: function (data) {
+        // TODO: filter, sort by date
+        $('.container').empty();
         $.each(data.data, function (i, item) {
-          //if (item.user.id === id) {
-            var image = {
-              userid: item.user.id,
-              username: item.user.username,
-              photo: item.images.low_resolution.url,
-              url: item.link,
-              tags: item.tags
-            };
-            var img = '<img src=' + image.photo + ' width="250" height="250"/>';
-            $('.container').append('<span class="photo">' + img + '</span>');
-            $('.photo').fadeIn('slow');
-          //}
+          var image = {
+            username: item.user.username,
+            photo: item.images.low_resolution.url,
+            url: item.link,
+            tags: item.tags,
+            location: item.location ? item.location.name : '',
+            likes: item.likes.count,
+            created: item.created_time,
+            caption: item.caption ? item.caption.text : ''
+          };
+          var img = '<img src="' + image.photo + '"/>';
+          $('.container').append('<span class="photo">' + img + '</span>');
         });
+        // animate photos once they're all loaded
+        $('.photo').fadeTo('slow', 1);
       },
       error: function (jqXHR, textStatus, errorThrown) {
         console.log("There was a problem loading the images!");
@@ -45,28 +44,30 @@ App.Config = {
 
   function createPhotoTile(item){
     var image = {
-      userid: item.user.id,
       username: item.user.username,
       photo: item.images.low_resolution.url,
       url: item.link,
-      tags: item.tags
+      tags: item.tags,
+      location: item.location ? item.location.name : "",
+      likes: item.likes.count,
+      created: item.created_time,
+      caption: item.caption ? item.caption.text : ""
     };
-
-    return '<img src=' + image.photo + ' width="250" height="250"/>';
+    var img = '<img src=' + image.photo + '/>';
+    // TODO: add jquery click event to flip and show information
   }
 
   function createPhotoList(photos){
     var photoList = '';
-    $('#photo-list').append(photoList)
-  }
-
-  function generateUrl(filter){
-    var url;
-    return url;
+    $('.container').append(photoList)
   }
 
   // public functions
-  .App = {
-    fetchPhotos: fetchPhotos;
+  App = {
+    fetchPhotos: fetchPhotos
   };
+
+  if ( typeof define === "function" && define.amd ) {
+    define( "app", [], function () { return App; } );
+  }
 }());
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,51 +1,29 @@
 require.config({
     paths: {
       'jquery': 'libs/jquery/jquery',
-      'lodash': 'libs/lodash/dist/lodash'
+      'lodash': 'libs/lodash/dist/lodash',
+      'app': 'app'
     },
     shim: {
       'lodash': {
+        deps: ['jquery']
+      },
+      'app': {
         deps: ['jquery']
       }
     }
 });
 
 
-require(['jquery', 'lodash'], function ($, _) {
+require(['jquery', 'lodash', 'app'], function ($, _, app) {
 
   $(document).ready(function () {
-    var hash = 'norfolkva';
-    var id = '23299063';
-    var client = '4c1191c3ee9040b9968f432f2c977964';
-    var count = 50;
-    var url = 'https://api.instagram.com/v1/tags/' + hash + '/media/recent?client_id=' +
-      client + '&count=' +   count;
-
-    $.ajax({
-      method: "GET",
-      url: url,
-      dataType: "jsonp",
-      cache: false,
-      success: function (data) {
-        $.each(data.data, function (i, item) {
-          //if (item.user.id === id) {
-            var image = {
-              userid: item.user.id,
-              username: item.user.username,
-              photo: item.images.low_resolution.url,
-              url: item.link,
-              tags: item.tags
-            };
-            var img = '<img src=' + image.photo + ' width="250" height="250"/>';
-            $('.container').append('<span class="photo">' + img + '</span>');
-            $('.photo').fadeIn('slow');
-          //}
-        });
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-        console.log("There was a problem loading the images!");
-      }
+    app.fetchPhotos("all");
+    // when any of the options are clicked
+    // fetch the pictures with the new filter
+    $('.option').click(function () {
+      var filter = $(this).text();
+      app.fetchPhotos(filter);
     });
-
   });
 });
