bcf8eaf8a9ab2ab0981e81e621a064620b4a2de2
Linda Nichols <lynnaloo@gmail.com>
2014-10-28 05:46:17 +0000

    resolves #8

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,6 @@
 var App = {};
 
 (function () {
-
   function fetchPhotos(filter) {
     var hash = filter,
       // TODO: move to config
@@ -17,24 +16,26 @@ var App = {};
       dataType: "jsonp",
       cache: false,
       success: function (data) {
-        // TODO: filter, sort by date
         $('.container').empty();
-        $.each(data.data, function (i, item) {
-          var image = {
-            username: item.user.username,
-            photo: item.images.low_resolution.url,
-            url: item.link,
-            tags: item.tags,
-            location: item.location ? item.location.name : '',
-            likes: item.likes.count,
-            created: item.created_time,
-            caption: item.caption ? item.caption.text : ''
-          };
-          var img = '<img src="' + image.photo + '"/>';
-          $('.container').append('<span class="photo">' + img + '</span>');
+        var images = filterImages(data.data, filter);
+        $.each(images, function (i, item) {
+          var photo = createPhotoTile(item);
+          // add flip animation
+          // TODO: this is buggy
+          $('.tile').click(function () {
+            $(this).find('.front').addClass('flipped').mouseleave(function () {
+              $(this).removeClass('flipped');
+            });
+            $(this).find('.back').addClass('flipped').mouseleave(function () {
+              $(this).removeClass('flipped');
+            });
+            return false;
+          });
+          // add this photo to the photo container
+          $('.container').append(photo);
         });
         // animate photos once they're all loaded
-        $('.photo').fadeTo('slow', 1);
+        $('.tile').fadeTo('slow', 1);
       },
       error: function (jqXHR, textStatus, errorThrown) {
         console.log("There was a problem loading the images!");
@@ -42,24 +43,37 @@ var App = {};
     });
   }
 
-  function createPhotoTile(item){
-    var image = {
-      username: item.user.username,
-      photo: item.images.low_resolution.url,
-      url: item.link,
-      tags: item.tags,
-      location: item.location ? item.location.name : "",
-      likes: item.likes.count,
-      created: item.created_time,
-      caption: item.caption ? item.caption.text : ""
-    };
-    var img = '<img src=' + image.photo + '/>';
-    // TODO: add jquery click event to flip and show information
-  }
+  function filterImages(data, filter) {
+    // TODO: implement sort
+    if (filter !== 'all') {
+      // TODO: lodash filter on tag
+    }
+    return data;
+  };
 
-  function createPhotoList(photos){
-    var photoList = '';
-    $('.container').append(photoList)
+  function createPhotoTile(item){
+    var username = '@' + item.user.username,
+      photo = item.images.low_resolution.url,
+      url = item.link,
+      location = item.location ? item.location.name || '' : '',
+      likes = item.likes.count + ' likes',
+      created = item.created_time ? new Date(item.created_time * 1000).toLocaleDateString() : '',
+      caption = item.caption ? item.caption.text || '' : '',
+      tags = _.map(item.tags, function (tag) {
+        return '#' + tag;
+      }),
+      img = '<img src="' + photo + '"/>';
+
+    var info = '<p>' + caption + '</p>' +
+        '<p>' + created + ' ' + username + '</p>' +
+        '<p>' + likes + '</p>' +
+        '<p>' + location + '</p>' +
+        '<p>' + tags.join(" ") + '</p>';
+
+
+    var tile = '<div class="front">' + img + '</div>' +
+      '<div class="back">' + info + '</div>';
+    return '<div class="tile-container"><div class="tile">' + tile + '</div></div>';
   }
 
   // public functions
@@ -67,6 +81,7 @@ var App = {};
     fetchPhotos: fetchPhotos
   };
 
+  // prepare module
   if ( typeof define === "function" && define.amd ) {
     define( "app", [], function () { return App; } );
   }
