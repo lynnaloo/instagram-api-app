c84b7bc3000d352872fe072a2c81f715ed3e1752
Linda Nichols <lynnaloo@gmail.com>
2014-11-01 18:57:32 +0000

    resolves #15, #16

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,43 +1,53 @@
 
 (function () {
-  function fetchPhotos() {
-    var baseUrl = 'https://api.instagram.com/v1/',
-      configs = {
-        id: '23299063',
-        client: '4c1191c3ee9040b9968f432f2c977964',
-        count: 50
-      };
+  var baseUrl = 'https://api.instagram.com/v1/',
+    configs = {
+      id: '23299063',
+      client: '4c1191c3ee9040b9968f432f2c977964',
+      count: 50
+    },
+    tags = {
+      'norfolkva': baseUrl + 'tags/norfolkva/media/recent?client_id=' + configs.client,
+      'fieldguidenfk': baseUrl + 'tags/fieldguidenfk/media/recent?client_id=' + configs.client,
+      'growinteractive': baseUrl + 'users/' + configs.id + '/media/recent?client_id=' + configs.client
+    },
+    selectedTags = {
+      'norfolkva': true,
+      'fieldguidenfk': true,
+      'growinteractive': true
+    };
 
-    var norfolkva = $.ajax({
-      method: "GET",
-      url: baseUrl + 'tags/norfolkva/media/recent?client_id=' + configs.client + '&count=' + configs.count,
-      dataType: "jsonp",
-      cache: false
-    });
-    var fieldguidenfk = $.ajax({
-      method: "GET",
-      url: baseUrl + 'tags/fieldguidenfk/media/recent?client_id=' + configs.client + '&count=' + configs.count,
-      dataType: "jsonp",
-      cache: false
-    });
-    var growinteractive = $.ajax({
-      method: "GET",
-      url: baseUrl + 'users/' + configs.id + '/media/recent?client_id=' + configs.client + '&count=' + configs.count,
-      dataType: "jsonp",
-      cache: false
-    });
+  function filterTags(tag) {
+    if (!tag) { return }
+    selectedTags[tag] = !selectedTags[tag];
+  };
 
+  function fetchPhotos(tag, callback) {
+    filterTags(tag);
+    var defers = _.map(selectedTags, function (selected, key) {
+        var ajax = $.ajax({
+          method: "GET",
+          url: tags[key] + '&count=' + configs.count,
+          dataType: "jsonp",
+          cache: false
+        });
+        return ajax;
+      });
     // empty the photo stream before fetching new photos
     $('.content').empty();
-    $.when.apply($, [norfolkva, fieldguidenfk, growinteractive]).then(function () {
-      var union = _.flatten(_.map(arguments, function (item) { return item[0].data; }));
+    // show loading image
+    $('.spinner').show();
+    $.when.apply($, defers).then(function () {
+      var union = _.flatten(_.map(arguments, function (item) {
+        return item[0].data;
+      }));
       var unique = _.uniq(union, 'id');
-
       _.each(unique, function (item) {
         var photo = createPhotoTile(item);
         // add this photo to the photo content div
         $('.content').append(photo);
       });
+      // TODO: sort
       // add tile flip animation on click
       $('.tile').click(function () {
         $(this).find('.front, .back').addClass('flipped');
@@ -49,6 +59,7 @@
       });
       // animate photos once they're all loaded
       $('.tile').fadeTo('slow', 1);
+      $('.spinner').hide();
     }, function () {
       console.log("Fetch of Instagram images has failed!");
     });
@@ -71,22 +82,23 @@
         '<p>' + created + ' ' + username + '</p>' +
         '<p>' + likes + '</p>' +
         '<p>' + location + '</p>' +
-        '<p>' + tags.join(" ") + '</p>';
+        '<p>' + tags.join(" ") + '</p>' +
+        '<p><a href="' + url + '" target="_blank">' + url + '</a></p>';
 
     var tile = '<div class="front">' + img + '</div>' +
       '<div class="back">' + info + '</div>';
     return '<div class="tile-container"><div class="tile">' + tile + '</div></div>';
-  }
+  };
 
   // public functions to expose to main and unit tests
   App = {
     fetchPhotos: fetchPhotos,
-    filters: []
+    filterTags: filterTags,
+    selectedTags: selectedTags
   };
 
   // prepare module
   if ( typeof define === "function" && define.amd ) {
     define( "app", [], function () { return App; } );
   }
-
 }());
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -18,19 +18,20 @@ require.config({
 require(['jquery', 'lodash', 'app'], function ($, _, app) {
 
   $(document).ready(function () {
-    app.fetchPhotos();
+    app.fetchPhotos(null);
     // when any of the options are clicked
     // fetch the pictures with the new hashtag
     $('.option').click(function () {
-      var tag = $(this).attr("title"); // the title attr of the link is the hashtag
       app.fetchPhotos(tag);
+      var tag = $(this).attr('title'); // the title attr of the link is the hashtag
+      $(this).toggleClass('checked'); // toggle the checked class
     });
     // when the toggle button is clicked, toggle icon and sidebar
     $('.toggle').click(function () {
-      $('.sidebar').toggle(function () {
+      $('.sidebar').toggle('fast', function () {
+        $('.right').toggleClass('open'); // toggle the margin class
+        $('.toggle').toggleClass('close'); // toggle the close/menu icon
       });
-        $('.right').toggleClass('open');
-        $(this).toggleClass('close');
     });
   });
 });
